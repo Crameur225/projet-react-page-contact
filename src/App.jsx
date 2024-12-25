b9582dc5b9bae2b5734647263d8a505f8c7e28cd
Dev <admin@example.com>
2024-12-25 20:47:52 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Contact from './components/Contact/Contact'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Contact/>
+    </div>
+  )
+}
+
+export default App
